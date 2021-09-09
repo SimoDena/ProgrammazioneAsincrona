ef2ebfaa69ede54a5aac2869a29da0f808fd0b22
Simone Denardi <simone.denardi@lasportiva.com>
2021-09-09 10:36:54 +0200

    Aggiungere i file di progetto.

---
+++ programmazioneAsincrona/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace programmazioneAsincrona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaluta = new System.Windows.Forms.Button();
+            this.lblSaluto = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSaluta
+            // 
+            this.btnSaluta.Location = new System.Drawing.Point(12, 12);
+            this.btnSaluta.Name = "btnSaluta";
+            this.btnSaluta.Size = new System.Drawing.Size(140, 64);
+            this.btnSaluta.TabIndex = 0;
+            this.btnSaluta.Text = "Saluta!";
+            this.btnSaluta.UseVisualStyleBackColor = true;
+            this.btnSaluta.Click += new System.EventHandler(this.btnSaluta_Click);
+            // 
+            // lblSaluto
+            // 
+            this.lblSaluto.AutoSize = true;
+            this.lblSaluto.Location = new System.Drawing.Point(12, 94);
+            this.lblSaluto.Name = "lblSaluto";
+            this.lblSaluto.Size = new System.Drawing.Size(0, 13);
+            this.lblSaluto.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(538, 94);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(250, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblSaluto);
+            this.Controls.Add(this.btnSaluta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaluta;
+        private System.Windows.Forms.Label lblSaluto;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ programmazioneAsincrona/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace programmazioneAsincrona
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string CalcolaSaluto()
+        {
+            string saluto = "Ciao a tutti!";
+            Thread.Sleep(5000);
+            return saluto;
+        }
+
+        private async void btnSaluta_Click(object sender, EventArgs e)
+        {
+
+            //Task<string> task = new Task<string>(CalcolaSaluto);
+            //task.Start();
+
+            var task = Task.Run(() => CalcolaSaluto()); // stessa cosa del codice sopra
+
+            lblSaluto.Text = "Calcolando Saluto....";
+            string saluto = await task;
+            lblSaluto.Text = saluto;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var progress = new Progress<int>(value =>
+            {
+                progressBar1.Value = value;
+                label1.Text = $"{value}%";
+            });
+
+            await Task.Run(() => LoopNumers(100, progress));
+
+            label1.Text = "Completato";
+        }
+
+        void LoopNumers(int count, IProgress<int> progress)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Thread.Sleep(100);
+                var percentComplete = (i * 100) / count;
+                progress.Report(percentComplete);
+            }
+        }
+    }
+}
